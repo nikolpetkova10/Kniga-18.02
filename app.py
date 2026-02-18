fd8979b6102a64e1e5818b3580ca0b746ad53f58
nikolpetkova10 <petkovaniki123@gmail.com>
2026-02-18 14:07:59 +0200

    Fix indentation for warning message in app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write("Enter a book title to chech if it exists in the database.")
 user_input = st.text_input("Book Title")
 if st.button("Check Book"):
            if user_input.strip() == "":
-           st.warning("Please enter a book title.")
+                      st.warning("Please enter a book title.")
 elif user_input in books:
            st.success("The book exists in the database.")
 else:
